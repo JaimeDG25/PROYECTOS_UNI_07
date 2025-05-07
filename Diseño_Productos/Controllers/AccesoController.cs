c3f33f4cf02d3020b58bcc7f52426c4542a3f569
jhair garcia <garciajhair22@gmail.com>
2025-05-07 12:58:09 -0500

    Agregar archivos de proyecto.

---
+++ Diseño_Productos/Controllers/AccesoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Diseño_Productos.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Adpoint()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
